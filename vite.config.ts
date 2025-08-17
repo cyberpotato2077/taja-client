8735b42392647be753238a6dd3d5c666565d6d7a
cyberpotato2077 <cyberpotato96@gmail.com>
2025-08-18 04:44:27 +0900

    build 수정 (#6)
    Co-authored-by: cyberpotato2077 <cyberpotato2077@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +15,9 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   base:"/taja-client/"
