3d6d2dec2c222fa689e72b033ba8dfe9c45def7e
cyberpotato2077 <cyberpotato96@gmail.com>
2025-08-24 20:23:52 +0900

    feat: drawer, top 추가 (#14)
    * drawer 추가
    
    * drawer 임시 추가...
    
    * 주석 추가
    
    * toss/overlay-kit 설치
    
    * overlay kit 적용
    
    * useoverlay 추가
    
    * useOverlay
    
    * 가로 제한 추가
    
    * top

---
+++ src/components/maps/station-drawer.tsx
@@ -0,0 +1,35 @@
+import { Button } from "../ui/button";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+} from "../ui/drawer";
+
+export function StationDrawer({
+	open,
+	close,
+}: { open: boolean; close: () => void }) {
+	return (
+		<Drawer open={open} onOpenChange={close}>
+			<DrawerContent className="max-w-screen-sm !left-1/2 !-translate-x-1/2">
+				<div className="mx-auto w-full max-w-sm">
+					<DrawerHeader>
+						<DrawerTitle>DD</DrawerTitle>
+						<DrawerDescription>FF</DrawerDescription>
+					</DrawerHeader>
+
+					<DrawerFooter>
+						<Button>Submit</Button>
+						<DrawerClose asChild>
+							<Button variant="outline">Cancel</Button>
+						</DrawerClose>
+					</DrawerFooter>
+				</div>
+			</DrawerContent>
+		</Drawer>
+	);
+}
