6e83f874ac9eb044793f27b20bbd90bd588340fd
cyberpotato2077 <cyberpotato2077@gmail.com>
2025-08-31 15:34:25 +0900

    apiKey 누락 안내

--- src/components/maps/station-map.tsx
+++ src/components/maps/station-map.tsx
@@ -9,9 +9,20 @@ import {
 
 export function StationMap() {
 	const overlay = useOverlay();
+
+	const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 	const position = { lat: 53.54992, lng: 10.00678 };
+
+	if (apiKey == null) {
+		return (
+			<div className="w-full h-full flex items-center justify-center min-h-[100vh] bg-gradient-to-b from-gray-300 to-white">
+				apiKey를 찾지 못했습니다.
+			</div>
+		);
+	}
+
 	return (
-		<APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+		<APIProvider apiKey={apiKey}>
 			<GoogleMap
 				className="fixed top-0 max-w-screen-sm w-full h-[100vh]"
 				defaultCenter={position}
