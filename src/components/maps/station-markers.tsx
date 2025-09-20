c0709f3dc7c1f682618725da696f95991bce3028
cyberpotato2077 <cyberpotato96@gmail.com>
2025-09-20 16:06:43 +0900

    목데이터 추가

--- src/components/maps/station-markers.tsx
+++ src/components/maps/station-markers.tsx
@@ -1,13 +1,33 @@
-import { http } from "@/utils/http";
+import { getNearbyStations } from "@/remotes/getNearbyStations";
 import { useQuery } from "@tanstack/react-query";
+import { AdvancedMarker } from "@vis.gl/react-google-maps";
 
 export function StationMarkers() {
 	const { data } = useQuery({
 		queryKey: ["stations"],
-		queryFn: () => http.get("/stations/map/nearby"),
+		queryFn: () =>
+			getNearbyStations({
+				latitude: 0,
+				latitudeDelta: 0,
+				longitude: 0,
+				longitudeDelta: 0,
+			}),
 	});
 
 	console.log(data);
 
-	return <></>;
+	return (
+		<>
+			{data?.map((station) => (
+				<AdvancedMarker
+					key={`${station.latitude}-${station.longitude}`}
+					position={{
+						lat: station.latitude,
+						lng: station.longitude,
+					}}
+					onClick={() => console.log(station)}
+				/>
+			))}
+		</>
+	);
 }
