8d2ca711feea60bb00afae64c91a6b0eed83b611
cyberpotato2077 <cyberpotato96@gmail.com>
2026-01-18 18:13:25 +0900

    디테일 추가

--- src/routes/station/$id.tsx
+++ src/routes/station/$id.tsx
@@ -1,4 +1,7 @@
 import { LayoutWithTop } from "@/components/layout-with-top";
+import { StationDetail } from "@/components/station-detail";
+import { stationQueryOptions } from "@/queries/station-query-options";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, useParams } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/station/$id")({
@@ -8,12 +11,19 @@ export const Route = createFileRoute("/station/$id")({
 function StationPage() {
 	const { id } = useParams({ from: Route.id });
 
+	const {
+		data: station,
+		isPending,
+		isError,
+	} = useQuery(stationQueryOptions.detail({ id: Number(id) }));
+
+	if (isPending || isError) {
+		return null;
+	}
+
 	return (
 		<LayoutWithTop showBackButton>
-			<div className="p-4">
-				<h1 className="text-xl font-bold">대여소 상세 정보</h1>
-				<p>Station ID: {id}</p>
-			</div>
+			<StationDetail station={station} />
 		</LayoutWithTop>
 	);
 }
