234927159b54ef00938d63d86dda0c1979323870
cyberpotato2077 <cyberpotato96@gmail.com>
2026-01-25 18:16:27 +0900

    width 제거

--- src/components/maps/station-drawer.tsx
+++ src/components/maps/station-drawer.tsx
@@ -1,6 +1,7 @@
 import { stationQueryOptions } from "@/queries/station-query-options";
 import { useQuery } from "@tanstack/react-query";
 import type { useNavigate } from "@tanstack/react-router";
+import { StationDetail } from "../station-detail";
 import { Button } from "../ui/button";
 import {
 	Drawer,
@@ -27,8 +28,8 @@ export function StationDrawer({
 }: StationDrawerProps) {
 	const {
 		data: station,
-		isLoading,
-		error,
+		isPending,
+		isError,
 	} = useQuery(stationQueryOptions.detail({ id: stationId }));
 
 	return (
@@ -53,49 +54,13 @@ export function StationDrawer({
 					}
 				}}
 			>
-				<div className="mx-auto w-full max-w-sm">
-					<DrawerHeader>
-						<DrawerTitle>
-							{isLoading
-								? "Loading..."
-								: error
-									? "Error"
-									: station?.name || "Station Details"}
-						</DrawerTitle>
-						<DrawerDescription>
-							{error
-								? error.message
-								: station?.address || "No station selected"}
-						</DrawerDescription>
-					</DrawerHeader>
-
-					{station && !isLoading && !error && (
-						<div className="p-4 pb-0 text-sm text-gray-700">
-							<p>
-								Latitude:{" "}
-								<span className="font-medium">{station.latitude}</span>
-							</p>
-							<p>
-								Longitude:{" "}
-								<span className="font-medium">{station.longitude}</span>
-							</p>
-							<p>
-								Available Bikes (Observed):{" "}
-								<span className="font-medium">
-									{station.todayAvailableBike?.observedBikeCountByHour?.slice(
-										-1,
-									)[0]?.bikeCount ?? "N/A"}
-								</span>
-							</p>
-							<p>
-								Available Bikes (Predicted):{" "}
-								<span className="font-medium">
-									{station.todayAvailableBike?.predictedBikeCountByHour?.slice(
-										-1,
-									)[0]?.bikeCount ?? "N/A"}
-								</span>
-							</p>
-						</div>
+				<div className="mx-auto w-full">
+					{isError ? (
+						<></>
+					) : isPending ? (
+						<></>
+					) : (
+						<StationDetail station={station} />
 					)}
 
 					<DrawerFooter>
