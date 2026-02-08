9ad0815265f0f220d7ccfa173a0351cc13a276d1
cyberpotato2077 <cyberpotato96@gmail.com>
2026-02-08 20:28:28 +0900

    불필요 파일 제거 (#72)

--- src/remotes/get-favorite-stations/handler.ts
+++
@@ -1,9 +0,0 @@
-import { http, HttpResponse } from "msw";
-import { mockFavoriteStations } from "./mock";
-
-export const getFavoriteStationsMock = http.get(
-	"/stations/map/favorites",
-	() => {
-		return HttpResponse.json(mockFavoriteStations);
-	},
-);
